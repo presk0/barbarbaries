11d035d6a5abe137a0b8c115bea0b555fa0ecbb9
presk0 <contact@presko.info>
2023-07-23 15:52:50 +0200

    NOT TESTED: counter next iplemented

--- rapsalgo.c
+++ rapsalgo.c
@@ -22,17 +22,11 @@ C (or F) : 	bab
 
 Notice :
 abb cannot be followed by any other b (because abbb contains 3 b)
-so A cannot be followed by 	_ _ _
-				A B C
-Whit the same concept _
-		      A cannot be followed by A B C
+	A cannot be followed by D E orF
+Whit the same concept 
+	B cannot be followed by E and E cannot be followed by B
 
-	B cannot be followed by _
-				B and _
-				      B cannot be followed by B
-
-	C cannot be followed by B and _    _
-				      C by B
+	C cannot be followed by B and F by E
 
 The infinite possibilities are parsed like digits
 A B C D E F
@@ -42,15 +36,26 @@ BA BB ...
 ...    FD (FE) FF
 AAA AAB ...
 	...
+
+counter is a list's list
 	*/
 #include <stdbool.h>
 #include <stdlib.h>
 #define MALLOC_ERR_NO 11
+
+
 typedef struct	s_node {
-	char	id;
-	bool	cell[3]; // true or false
+	void			*data;
 	struct s_node	*next;
+	struct s_node	*prev;
 } t_node;
+
+t_node *reel;
+
+typedef struct	s_triade {
+	char	id; // A B C D E or F
+	bool	cell[3]; // true or false
+} t_triade;
 
 t_node *set_list() {
 	// create chained list with A B C D E F
@@ -58,53 +63,84 @@ t_node *set_list() {
 	t_node	*last;
 	t_node	*new;
 	head = malloc (sizeof(t_node));
-	head->cell[0] = true;
-	head->cell[1] = false;
-	head->cell[2] = false;
-	head->id = 'A';
+	((t_triade*)head->data)->cell[0] = true;
+	((t_triade*)head->data)->cell[1] = false;
+	((t_triade*)head->data)->cell[2] = false;
+	((t_triade*)head->data)->id = 'A';
 	last = malloc (sizeof(t_node));
 	if (!last)
 		exit(MALLOC_ERR_NO);
-	last->cell[0] = true;
-	last->cell[1] = true;
-	last->cell[2] = false;
-	head->id = 'B';
+	((t_triade*)last->data)->cell[0] = true;
+	((t_triade*)last->data)->cell[1] = true;
+	((t_triade*)last->data)->cell[2] = false;
+	((t_triade*)last->data)->id = 'B';
 	head->next = last;
+	last->prev = head;
 	new = malloc (sizeof(t_node));
 	if (!new)
 		exit(MALLOC_ERR_NO);
-	new->cell[0] = true;
-	new->cell[1] = false;
-	new->cell[2] = true;
-	head->id = 'C';
+	((t_triade*)new->data)->cell[0] = true;
+	((t_triade*)new->data)->cell[1] = false;
+	((t_triade*)new->data)->cell[2] = true;
+	((t_triade*)new->data)->id = 'C';
 	last->next = new;
+	new->prev = last;
 	last = new;
 	new = malloc (sizeof(t_node));
 	if (!new)
 		exit(MALLOC_ERR_NO);
-	new->cell[0] = false;
-	new->cell[1] = true;
-	new->cell[2] = true;
-	head->id = 'D';
+	((t_triade*)new->data)->cell[0] = false;
+	((t_triade*)new->data)->cell[1] = true;
+	((t_triade*)new->data)->cell[2] = true;
+	((t_triade*)new->data)->id = 'D';
 	last->next = new;
+	new->prev = last;
 	last = new;
 	new = malloc (sizeof(t_node));
 	if (!new)
 		exit(MALLOC_ERR_NO);
-	new->cell[0] = false;
-	new->cell[1] = false;
-	new->cell[2] = true;
-	head->id = 'E';
+	((t_triade*)new->data)->cell[0] = false;
+	((t_triade*)new->data)->cell[1] = false;
+	((t_triade*)new->data)->cell[2] = true;
+	((t_triade*)new->data)->id = 'E';
 	last->next = new;
+	new->prev = last;
 	last = new;
 	new = malloc (sizeof(t_node));
 	if (!new)
 		exit(MALLOC_ERR_NO);
-	new->cell[0] = false;
-	new->cell[1] = true;
-	new->cell[2] = false;
-	head->id = 'F';
+	((t_triade*)new->data)->cell[0] = false;
+	((t_triade*)new->data)->cell[1] = true;
+	((t_triade*)new->data)->cell[2] = false;
+	((t_triade*)new->data)->id = 'F';
 	last->next = new;
 	new-> next = head;
+	new->prev = last;
+	head->prev = new ;
 	return head;
+}
+
+t_node	*counter_next(t_node *counter) {
+	bool	retenue = false;
+	do {
+		if (counter) {
+			if (((t_triade *)((t_node *)counter->data)->data)->id == 'F')
+				retenue = true;
+			else retenue = false;
+			((t_node *)counter->data)->data = ((t_node *)((t_node *)counter->data)->data)->next;
+			if (retenue && !counter->next) {
+				counter->next = malloc(sizeof(t_node));
+				if (!counter->next)
+					exit(MALLOC_ERR_NO);
+				counter->next->next = NULL;
+				((t_node *)(counter->data))->data = (t_node *)reel;
+				retenue = false;
+			} else if (retenue && counter->next)
+			counter = counter->next;
+		}
+	} while (retenue) ;
+}
+
+int main () {
+	reel = set_list();
 }
